1c3f1a0ede7e47ba03f813537e5a9e5744f0b313
lively-teacher-9822 <ccrive10@gmail.com>
2018-12-05 21:24:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+//the test is asking me to enter a function called takeNumber that accepts two parameters which I will name currentLine, name)
+
+function takeNumber(currentLine,name){
+  currentLine.push("Ada");
+  return 
+}
+// I know the test is asking to add a person names Ada
